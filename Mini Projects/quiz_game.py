670aa1e23b682483bb1c6e58eea2cb6a543f7af7
BusinessCoffee <levi.businesscoffee@gmail.com>
2024-03-25 22:14:11 -0700

    Added number guesses

--- Mini Projects/quiz_game.py
+++ Mini Projects/quiz_game.py
@@ -1,31 +1,39 @@
 print("Weclome to my computer quiz!")
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
+score = 0
 print("Okay! Let's play ;)")
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
-if answer == "graphics processing unit":
+if answer.lower() == "graphics processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for? ")   
-if answer == "random access memory":
+if answer.lower() == "random access memory":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does PSU stand for? ")
-if answer == "power supply":
+if answer.lower() == "power supply":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+    
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
