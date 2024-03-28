cb6d32dce0b5a1ac2c2f1fae4464f1196aa1cd8f
BusinessCoffee <levi.businesscoffee@gmail.com>
2024-03-28 16:23:12 -0700

    Turtle racing is done

---
+++ Medium/Mini Projects/turtle_race.py
@@ -0,0 +1,69 @@
+import turtle
+import random
+import time
+
+WIDTH, HEIGHT = 500, 500
+COLORS = ["red", "green", "blue", "orange", "yellow", "white", "purple", "pink", "brown", "cyan"]
+
+def get_number_of_racers():
+    racers = 0
+    
+    while True:
+        racers = input("Enter the number of racers (2-10): ")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print("Please enter a number.")
+            continue
+        
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("Please enter a number within the range (2-10).")
+
+def race(colors):
+    turtles = create_turtles(colors)
+    
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+            
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10:
+                return colors[turtles.index(racer)]
+
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape("turtle")
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 20)
+        racer.pendown()
+        turtles.append(racer)
+    return turtles
+
+def init_turtle():
+    wn = turtle.Screen()
+    
+    rootwindow = wn.getcanvas().winfo_toplevel()
+    rootwindow.call('wm', 'attributes', '.', '-topmost', '1')
+    rootwindow.call('wm', 'attributes', '.', '-topmost', '0')
+    
+    wn.setup(WIDTH, HEIGHT)
+    wn.title("Turtle Racing!")
+    wn.bgcolor("black")
+
+racers = get_number_of_racers()
+init_turtle()
+
+random.shuffle(COLORS)
+colors = COLORS[:racers]
+
+winner = race(colors)
+print("The winner is the turtle with color:", winner)
+time.sleep(5)
